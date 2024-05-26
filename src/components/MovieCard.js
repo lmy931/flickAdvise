df220ebc9913eb22de891d39d457fe96b56ba9ee
Li Mengyu <aumengyu@gmail.com>
2024-05-26 23:43:28 +0930

    update

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,13 +10,12 @@ function MovieCard({ title, cast, type, description, imageSrc, featured = true }
           <img className="bd-placeholder-img" width="auto" height="250" src={`${process.env.REACT_APP_BASE_URL}/${imageSrc}`} alt={title} />
         </div>
         <div className="col p-4 d-flex flex-column position-static m-text-block">
-          <strong className="d-inline-block mb-2 text-primary-emphasis">{type}</strong>
+          <strong className="d-inline-block mb-2 text-primary-emphasis m-type-text">{type}</strong>
           <h3 className="mb-0 m-title-text">{title}</h3>
           <div className="mb-1 m-cast-text">{cast}</div>
           <p className="card-text mb-auto text-body-secondary">{description}</p>
-          <a href="#" className="icon-link gap-1 icon-link-hover stretched-link">
+          <a href="#" className="watch-link">
             在线观看
-            <svg className="bi"><use xlinkHref="#chevron-right"></use></svg>
           </a>
         </div>
       </div>
